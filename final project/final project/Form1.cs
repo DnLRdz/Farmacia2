bef44258053c2a711193d1e204dd9af50d511ce4
DnLRdz <71399970+DnLRdz@users.noreply.github.com>
2020-11-20 14:16:37 -0600

    Update Form1.cs

--- final project/final project/Form1.cs
+++ final project/final project/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,8 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
-namespace final_project
+namespace farmacia
 {
     public partial class Form1 : Form
     {
@@ -16,8 +17,42 @@ namespace final_project
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            string connect="datasource=localhost;port33068;username=Dnlpq66;password=irhN3e0c1Tzu;database=farmacia";
+            string query = "select * from login where user = '"+textBox1.Text+"'AND password ='"+textBox2.Text+"'";
+            MySqlConnection databaseConnection = new MySqlConnection(connect);
+            MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
+            commandDatabase.CommandTimeout = 60;
+            MySqlDataReader reader;
+
+            try
+            {
+                databaseConnection.Open();
+                reader = commandDatabase.ExecuteReader();
+            }
+
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+            
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
-prube4
 
-    asdas
